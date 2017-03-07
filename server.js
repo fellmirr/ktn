e70ad96810618df8e0d480f6d94e551ab445c253
Håkon Harnes <sosser@skymiastudios.com>
2017-03-07 21:19:20 +0100

    Initial client handling in server
    Added some initial attempt at handling clients in server

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const net = require('net');
 
 const port = 3000;
 
+users = {};
+
 const server = net.createServer((socket) => {
     socket.setEncoding("utf8");
-    socket.on('data', handleData);
+    socket.on('data', (data) => {handleData(socket.id, data)});
 }).on('error', (err) => {
     //Handle errors
 });
@@ -16,6 +18,13 @@ server.listen({
     console.log("Listening on port " + port);
 });
 
-function handleData(data) {
+function handleData(clientid, data) {
     console.log(data);
+    var data = JSON.parse(data);
+
+    if (data.command == "login") {
+        users[clientid] = data.content;
+
+        console.log(users);
+    }
 }
