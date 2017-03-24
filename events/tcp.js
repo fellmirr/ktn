691e33ec6447d739ce9fcbcb25f5d1f3d9184712
Håkon Harnes <sosser@skymiastudios.com>
2017-03-24 14:14:51 +0100

    Cleanup
    Remove old files

--- events/tcp.js
+++
@@ -1,8 +0,0 @@
-var EventEmitter = require("events").EventEmitter;
-
-module.exports = class TCP { 
-    constructor() {
-        this.events = new EventEmitter();
-        this.events.emit('someEvent');
-    }
-}
